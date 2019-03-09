ea8e9bb5bfc42659611e68f8bca5435cfd996810
Ilya Zarubin <air_hamster@bk.ru>
2019-03-09 21:11:07 +0300

    changed to proper names

---
+++ sd_modules.mk
@@ -0,0 +1,9 @@
+# List of all the board related files.
+SD_SRC = ../sd_modules/shell_cmd_common/board.c
+
+# Required include directories
+SD_INC = ../sd_modules/shell_cmd_common
+
+# Shared variables
+ALLCSRC += $(SD_SRC)
+ALLINC  += $(SD_INC)
