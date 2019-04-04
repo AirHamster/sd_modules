ce1c368ce7d567f6b76324d3c58dd7fb2f98c110
Ilya Zarubin <air_hamster@bk.ru>
2019-04-04 11:04:51 +0300

    nmea disabled

--- neo-m8/neo-m8.c
+++ neo-m8/neo-m8.c
@@ -22,10 +22,22 @@ void neo_write_byte(SPIDriver *SPID, uint8_t reg_addr, uint8_t value) {
 	palClearLine(LINE_GREEN_LED); /* LED OFF.*/
 }
 
+void neo_write(SPIDriver *SPID, uint8_t *txbuff, uint8_t len) {
+	spiAcquireBus(SPID);              /* Acquire ownership of the bus.    */
+	//palSetLine(LINE_GREEN_LED); /* LED ON.                          */
+	palClearLine(LINE_NEO_CS);
+	chThdSleepMilliseconds(1);
+	spiSend(SPID, len, txbuff); /* send request       */
+	spiReleaseBus(SPID); /* Ownership release.               */
+	palSetLine(LINE_NEO_CS);
+	chThdSleepMilliseconds(1);
+//	palClearLine(LINE_GREEN_LED); /* LED OFF.*/
+}
+
 uint8_t neo_read_byte(SPIDriver *SPID, uint8_t reg_addr) {
 	uint8_t value;
 	reg_addr |= 0x80;	//0x80 indicates read operation
-	palSetLine(LINE_GREEN_LED); /* LED ON.                          */
+	//palSetLine(LINE_GREEN_LED); /* LED ON.                          */
 	spiAcquireBus(SPID);              /* Acquire ownership of the bus.    */
 	palClearLine(LINE_NEO_CS);
 	chThdSleepMilliseconds(1);
@@ -34,14 +46,14 @@ uint8_t neo_read_byte(SPIDriver *SPID, uint8_t reg_addr) {
 	spiReleaseBus(SPID); /* Ownership release.               */
 	palSetLine(LINE_NEO_CS);
 	chThdSleepMilliseconds(1);
-	palClearLine(LINE_GREEN_LED); /* LED OFF.*/
+	//palClearLine(LINE_GREEN_LED); /* LED OFF.*/
 	return value;
 }
 
 void neo_read_bytes(SPIDriver *SPID, uint16_t num, uint8_t *rxbuf) {
 	uint8_t *txbuf[num];
 	memset(txbuf, 0xFF, 100);
-	palSetLine(LINE_GREEN_LED); /* LED ON.                          */
+	//palSetLine(LINE_GREEN_LED); /* LED ON.                          */
 	spiAcquireBus(SPID);              /* Acquire ownership of the bus.    */
 	palClearLine(LINE_NEO_CS);
 	chThdSleepMilliseconds(1);
@@ -50,7 +62,7 @@ void neo_read_bytes(SPIDriver *SPID, uint16_t num, uint8_t *rxbuf) {
 	spiReleaseBus(SPID); /* Ownership release.               */
 	palSetLine(LINE_NEO_CS);
 	chThdSleepMilliseconds(1);
-	palClearLine(LINE_GREEN_LED); /* LED OFF.*/
+	//palClearLine(LINE_GREEN_LED); /* LED OFF.*/
 }
 
 /* Brief: function that finds sequence of 50 0xFF bytes
@@ -78,4 +90,54 @@ int8_t neo_find_ff(uint8_t *buff, uint8_t num){
 	}else{
 		return -1;
 	}
+}
+
+void neo_switch_to_ubx(void){
+	uint8_t packet[28];
+	uint8_t len = 28;
+	memset(packet, 0, 28);
+	neo_apply_header(packet, UBX_HEADER);
+	neo_apply_class(packet, UBX_CFG_CLASS);
+	neo_apply_id(packet, UBX_CFG_PRT_ID);
+	neo_apply_length(packet, UBX_CFG_PRT_LENGTH);
+	packet[6] = 4;
+	packet[19] = (1 << 0) | (1 << 5);
+	packet[21] = (1 << 0) | (1 << 5);
+	neo_apply_crc(packet, len);
+	int i;
+	chprintf((BaseSequentialStream*)&SD1, "NMEA OFF: \n\r");
+	for (i = 0; i< len; i++){
+		chprintf((BaseSequentialStream*)&SD1, "%x ", packet[i]);
+	}
+	chprintf((BaseSequentialStream*)&SD1, ":\n\r");
+	neo_write(&SPID2, packet, len);
+}
+
+void neo_apply_header(uint8_t *buffer, uint16_t header){
+	buffer[0] = header >> 8;
+	buffer[1] = header & 0xFF;
+}
+void neo_apply_class(uint8_t *buffer, uint8_t class){
+	buffer[2] = class;
+}
+void neo_apply_id(uint8_t *buffer, uint8_t id){
+	buffer[3] = id;
+}
+// Little-endian
+void neo_apply_length(uint8_t *buffer, uint8_t len){
+	buffer[4] = len;
+	buffer[5] = 0;
+}
+void neo_apply_crc(uint8_t *buffer, uint8_t len){
+	uint8_t crc_a = 0;
+	uint8_t crc_b = 0;
+	uint8_t i;
+	for(i = 2; i < len-2; i++)
+	{
+		crc_a = crc_a + buffer[i];
+		crc_b = crc_b + crc_a;
+	}
+	buffer[len - 1] = crc_b;
+	buffer[len - 2] = crc_a;
+
 }
--- neo-m8/neo-m8.h
+++ neo-m8/neo-m8.h
@@ -18,8 +18,20 @@
 #include "chprintf.h"
 #include "minmea.h"
 
+#define UBX_HEADER			0xB562
+#define UBX_CFG_CLASS		0x06
+#define UBX_CFG_PRT_ID		0x00
+#define UBX_CFG_PRT_LENGTH	20
+
+
 void neo_write_byte(SPIDriver *SPID, uint8_t reg_addr, uint8_t value);
 uint8_t neo_read_byte(SPIDriver *SPID, uint8_t reg_addr);
 void neo_read_bytes(SPIDriver *SPID, uint16_t num, uint8_t *rxbuf);
+void neo_switch_to_ubx(void);
 
+void neo_apply_header(uint8_t *buffer, uint16_t header);
+void neo_apply_class(uint8_t *buffer, uint8_t class);
+void neo_apply_id(uint8_t *buffer, uint8_t id);
+void neo_apply_length(uint8_t *buffer, uint8_t len);
+void neo_apply_crc(uint8_t *buffer, uint8_t len);
 #endif /* SD_MODULES_NEO_M8_NEO_M8_H_ */
