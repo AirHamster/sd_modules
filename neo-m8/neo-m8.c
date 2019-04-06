ac7eb102dd7323f2f95a6442af43b6520ab4f03d
Ilya Zarubin <air_hamster@bk.ru>
2019-04-06 20:05:09 +0300

    pvt reading via ubx

--- neo-m8/neo-m8.c
+++ neo-m8/neo-m8.c
@@ -7,6 +7,7 @@
 
 #include "neo-m8.h"
 
+extern nav_pvt_t *pvt_box;
 void neo_write_byte(SPIDriver *SPID, uint8_t reg_addr, uint8_t value) {
 	uint8_t txbuf[2];
 	txbuf[0] = reg_addr;
@@ -63,6 +64,35 @@ void neo_read_bytes(SPIDriver *SPID, uint16_t num, uint8_t *rxbuf) {
 	palSetLine(LINE_NEO_CS);
 	chThdSleepMilliseconds(1);
 	//palClearLine(LINE_GREEN_LED); /* LED OFF.*/
+}
+
+void neo_polling(SPIDriver *SPID, uint8_t class, uint8_t id) {
+	uint8_t packet[0 + 8];
+	uint8_t len = 0 + 8;
+	uint16_t crc;
+	memset(packet, 0, 0 + 8);
+	neo_apply_header(packet, UBX_HEADER);
+	neo_apply_class(packet, class);
+	neo_apply_id(packet, id);
+	neo_apply_length(packet, 0);
+	//packet[UBX_PAYLOAD + 0] = UBX_NAV_CLASS;
+	//packet[UBX_PAYLOAD + 1] = UBX_NAV_PVT_ID;
+	//packet[UBX_PAYLOAD + 2] = 1;
+	crc = neo_calc_crc(packet, len);
+	packet[len-2] = crc >> 8;
+	packet[len-1] = crc & 0xFF;
+	int i;
+	chprintf((BaseSequentialStream*)&SD1, "POLLING: \n\r");
+			for (i = 0; i< len; i++){
+				chprintf((BaseSequentialStream*)&SD1, "%x ", packet[i]);
+			}
+			chprintf((BaseSequentialStream*)&SD1, "\n\r");
+	//chprintf((BaseSequentialStream*)&SD1, "PVT_ON: \n\r");
+	//for (i = 0; i< len; i++){
+	//	chprintf((BaseSequentialStream*)&SD1, "%x ", packet[i]);
+	//}
+	//chprintf((BaseSequentialStream*)&SD1, "\n\r");
+	neo_write(SPID, packet, len);
 }
 
 /* Brief: function that finds sequence of 50 0xFF bytes
@@ -192,4 +222,92 @@ void neo_poll_prt(void){
 		}
 		chprintf((BaseSequentialStream*)&SD1, "\n\r");
 		neo_write(&SPID2, packet, len);
+}
+void neo_poll_nav_pvt(void){
+	const uint16_t pack_len = (UBX_NAV_PVT_LEN + UBX_PAYLOAD + CRC_LEN);
+	uint8_t pvt_message[pack_len*2];
+	//uint8_t txbuff[pack_len];
+	uint8_t i, j;
+	uint16_t crc;
+	//memset(txbuff, 0xFF, pack_len);
+	//neo_polling(&SPID2, UBX_NAV_CLASS, UBX_NAV_PVT_ID);
+	chThdSleepMilliseconds(100);
+	neo_read_bytes(&SPID2, pack_len*2, pvt_message);
+	//chprintf((BaseSequentialStream*)&SD1, "SPI: ");
+		//    			    for (j = i; j < 120; j++){
+		  //  			    	chprintf((BaseSequentialStream*)&SD1, "%x ", pvt_message[j]);
+		    //			    }
+		    	//		    chprintf((BaseSequentialStream*)&SD1, "\n\r");
+	//spiExchange(&SPID2, pack_len, txbuff, pvt_message);
+	    for (i = 0; i < pack_len*2-1; i++)
+	    {
+	    	if ((pvt_message[i] == 0xB5) && (pvt_message[i+1] == 0x62)
+	    			&& (pvt_message[i+2] == 0x01) && (pvt_message[i+3] == 0x07)){
+	    		neo_cp_to_struct(&pvt_message[i], pvt_box);
+	    		crc = ((pvt_message[i + pack_len-2] << 8) | (pvt_message[i +pack_len-1]));
+	    	//	chprintf((BaseSequentialStream*)&SD1, "i is %d \n\r", i);
+	    		if (crc == neo_calc_crc(&pvt_message[i], pack_len)){
+	    			chprintf((BaseSequentialStream*)&SD1, "CRC is the same \n\r");
+	    			break;
+	    		}else{
+	    			chprintf((BaseSequentialStream*)&SD1, "CRC fault: %x vs %x \n\r", crc, neo_calc_crc(&pvt_message[i], pack_len));
+	    			break;
+	    		}
+	    	}
+	    }
+}
+void neo_cp_to_struct(uint8_t *msg, nav_pvt_t *pvt){
+	memcpy(pvt, &msg[6], 92);
+}
+
+int32_t neo_get_lat(nav_pvt_t *pvt){
+	return (reverse32(pvt->lat));
+}
+
+int32_t neo_get_lon(nav_pvt_t *pvt){
+	return (reverse32(pvt->lon));
+}
+
+uint8_t neo_get_numsv(nav_pvt_t *pvt){
+	return pvt->numSV;
+}
+
+uint8_t neo_get_valid(nav_pvt_t *pvt){
+	return pvt->valid;
+}
+
+int32_t neo_get_ground_speed(nav_pvt_t *pvt){
+	return (reverse32(pvt->gSpeed));
+}
+
+int16_t reverse16(int16_t x)
+{
+    x = (x & 0xFF) << 8 | (x & 0xFF00) >>  8;
+    return x;
+}
+
+int32_t reverse32(int32_t num)
+{
+//	int32_t b;
+	//b = x >> 24;
+	//b |= ((x >> 8) & 0x0000FF00);
+	//b |= ((x << 8) & 0x00FF0000);
+	//b |= ((x >> 24) & 0xFF000000);
+    //x = (x & 0x00FF00FF) <<  8 | (x & 0xFF00FF00) >>  8;
+    //x = (x & 0x0000FFFF) << 16 | (x & 0xFFFF0000) >> 16;
+	//int32_t num = 9;
+	int32_t b0,b1,b2,b3,b4,b5,b6,b7;
+	int32_t res = 0;
+
+	b0 = (num & 0xf) << 28;
+	b1 = (num & 0xf0) << 24;
+	b2 = (num & 0xf00) << 20;
+	b3 = (num & 0xf000) << 16;
+	b4 = (num & 0xf0000) << 12;
+	b5 = (num & 0xf00000) << 8;
+	b6 = (num & 0xf000000) << 4;
+	b7 = (num & 0xf0000000) << 4;
+
+	res = b0 + b1 + b2 + b3 + b4 + b5 + b6 + b7;
+    return res;
 }
--- neo-m8/neo-m8.h
+++ neo-m8/neo-m8.h
@@ -21,6 +21,7 @@
 #define RTCM3_EN			0
 
 #define UBX_PAYLOAD			6
+#define CRC_LEN				2
 #define UBX_HEADER			0xB562
 #define UBX_CFG_CLASS		0x06
 #define UBX_CFG_PRT_ID		0x00
@@ -31,16 +32,85 @@
 #define UBX_NAV_CLASS			0x01
 #define UBX_NAV_PVT_ID			0x07
 #define UBX_NAV_PVT_LEN			92
+
+typedef uint8_t		U1;
+typedef uint8_t		RU1_3;
+typedef int8_t		I1;
+typedef uint8_t		X1;
+typedef uint16_t	U2;
+typedef int16_t 	I2;
+typedef uint16_t	X2;
+typedef uint32_t	U4;
+typedef int32_t		I4;
+typedef uint32_t	X4;
+typedef float		R4;
+typedef double		R8;
+typedef char		CH;
+
+typedef struct{
+	U4 iTOW;
+	U2 year;
+	U1 month;
+	U1 day;
+	U1 hour;
+	U1 min;
+	U1 sec;
+	X1 valid;
+	U4 tAcc;
+	I4 nano;
+	U1 fixType;
+	X1 flags;
+	X1 flags2;
+	U1 numSV;
+	I4 lon;
+	I4 lat;
+	I4 height;
+	I4 hMSK;
+	U4 hAcc;
+	U4 vAcc;
+	I4 velN;
+	I4 velE;
+	I4 velD;
+	I4 gSpeed;
+	I4 headMot;
+	U4 sAcc;
+	U4 headAcc;
+	U2 pDOP;
+	U1 res1;
+	U1 res2;
+	U1 res3;
+	U1 res4;
+	U1 res5;
+	U1 res6;
+	I4 headVeh;
+	I2 magDec;
+	U2 magAcc;
+
+}nav_pvt_t;
+
+
 void neo_write_byte(SPIDriver *SPID, uint8_t reg_addr, uint8_t value);
 uint8_t neo_read_byte(SPIDriver *SPID, uint8_t reg_addr);
 void neo_read_bytes(SPIDriver *SPID, uint16_t num, uint8_t *rxbuf);
 void neo_switch_to_ubx(void);
 void neo_set_pvt_1hz(void);
 void neo_poll_prt(void);
+void neo_poll_nav_pvt(void);
+void neo_polling(SPIDriver *SPID, uint8_t class, uint8_t id);
+void neo_cp_to_struct(uint8_t *msg, nav_pvt_t *pvt);
 
 void neo_apply_header(uint8_t *buffer, uint16_t header);
 void neo_apply_class(uint8_t *buffer, uint8_t class);
 void neo_apply_id(uint8_t *buffer, uint8_t id);
 void neo_apply_length(uint8_t *buffer, uint8_t len);
 uint16_t neo_calc_crc(uint8_t *buffer, uint8_t len);
+
+int32_t neo_get_lat(nav_pvt_t *pvt);
+int32_t neo_get_lon(nav_pvt_t *pvt);
+uint8_t neo_get_numsv(nav_pvt_t *pvt);
+uint8_t neo_get_valid(nav_pvt_t *pvt);
+int32_t neo_get_ground_speed(nav_pvt_t *pvt);
+int16_t reverse16(int16_t x);
+int32_t reverse32(int32_t x);
+
 #endif /* SD_MODULES_NEO_M8_NEO_M8_H_ */
